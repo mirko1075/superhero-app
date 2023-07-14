ab920a8f6babdf1af310a3b37389246e1f57e131
MirkoNexplore <mirko.siddi@nexplore.com>
2023-07-14 09:53:42 +0200

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 import { AddHeroComponent } from './components/add-hero/add-hero.component';
 import { HeroDetailsComponent } from './components/hero-details/hero-details.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesListComponent,
     AddHeroComponent,
-    HeroDetailsComponent
+    HeroDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
